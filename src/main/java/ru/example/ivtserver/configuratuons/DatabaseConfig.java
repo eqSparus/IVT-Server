92ddd5c53fd7013d31b3772380981177477666f1
eqSparus <sparus-1693@yandex.ru>
2023-02-21 01:25:43 +0600

    Добавлен аудитор времени

--- src/main/java/ru/example/ivtserver/configuratuons/DatabaseConfig.java
+++ src/main/java/ru/example/ivtserver/configuratuons/DatabaseConfig.java
@@ -2,16 +2,23 @@ package ru.example.ivtserver.configuratuons;
 
 import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
-import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.couchbase.config.AbstractCouchbaseConfiguration;
 import org.springframework.data.couchbase.repository.auditing.EnableCouchbaseAuditing;
+import org.springframework.data.couchbase.repository.config.EnableCouchbaseRepositories;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import java.time.ZonedDateTime;
+import java.util.Optional;
 
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @Configuration
 @EnableCouchbaseAuditing
-@Log4j2
+@EnableTransactionManagement
+@EnableCouchbaseRepositories(basePackages = "ru.example.ivtserver.repositories")
 public class DatabaseConfig extends AbstractCouchbaseConfiguration {
 
     @Value("${database.connection-string}")
@@ -46,4 +53,21 @@ public class DatabaseConfig extends AbstractCouchbaseConfiguration {
         return bucketName;
     }
 
+    @Override
+    protected boolean autoIndexCreation() {
+        return true;
+    }
+
+    @Bean
+    public AuditorAware<ZonedDateTime> auditorAware(){
+        return new ZoneDateTimeAuditorAware();
+    }
+
+    private static class ZoneDateTimeAuditorAware implements AuditorAware<ZonedDateTime>{
+
+        @Override
+        public Optional<ZonedDateTime> getCurrentAuditor() {
+            return Optional.of(ZonedDateTime.now());
+        }
+    }
 }
