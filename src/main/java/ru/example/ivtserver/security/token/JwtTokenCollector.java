e1fd69702e42e61b038d600c0dd5df2b1572d8b8
eqSparus <sparus-1693@yandex.ru>
2023-02-21 01:33:40 +0600

    Добавлены токены jwt

--- src/main/java/ru/example/ivtserver/security/token/JwtTokenCollector.java
+++ src/main/java/ru/example/ivtserver/security/token/JwtTokenCollector.java
@@ -1,2 +1,116 @@
-package ru.example.ivtserver.security.token;public class JwtTokenCollector {
+package ru.example.ivtserver.security.token;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Service
+@Log4j2
+public class JwtTokenCollector implements TokenCollector {
+
+    @Value("${security.token.jwt.valid-time-second}")
+    Long tokenValidTime;
+
+    @Value("${security.token.jwt.bearer}")
+    String prefixBearer;
+
+    @Value("${security.token.jwt.header-authorization}")
+    String tokenHeader;
+
+    final SecretKey key;
+
+    public JwtTokenCollector(Environment env) {
+        this.key = Keys.hmacShaKeyFor(env.getRequiredProperty("security.token.jwt.access")
+                .getBytes(StandardCharsets.UTF_8));
+    }
+
+    @NonNull
+    @Override
+    public String generateToken(@NonNull String email) {
+        return generateToken(email, tokenValidTime);
+    }
+
+    @NonNull
+    @Override
+    public String generateToken(@NonNull String email, @NonNull Long time) {
+
+        var now = LocalDateTime.now();
+        var timeStop = now.plusSeconds(time)
+                .atZone(ZoneId.systemDefault()).toInstant();
+
+        return prefixBearer + Jwts.builder()
+                .setSubject(email)
+                .setExpiration(Date.from(timeStop))
+                .signWith(key)
+                .compact();
+    }
+
+    @NonNull
+    @Override
+    public String getToken(@NonNull HttpServletRequest request) {
+        var header = request.getHeader(tokenHeader);
+        return getToken(header);
+    }
+
+    @NonNull
+    @Override
+    public String getToken(@NonNull String token) {
+        return token.replace(prefixBearer, "");
+    }
+
+    @Override
+    public boolean isValidToken(@NonNull String token) {
+
+        var parser = Jwts.parserBuilder()
+                .setSigningKey(key).build();
+
+        try {
+            return !parser
+                    .parseClaimsJwt(token)
+                    .getBody()
+                    .getExpiration()
+                    .before(new Date());
+        } catch (ExpiredJwtException e) {
+            log.error("Время жизни токена истекло", e);
+        }
+
+        return false;
+    }
+
+    @NonNull
+    @Override
+    public String getEmailFromToken(@NonNull String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJwt(token)
+                .getBody()
+                .getSubject();
+    }
+
+    @NonNull
+    @Override
+    public Claims getBody(@NonNull String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJwt(token)
+                .getBody();
+    }
 }
--- src/main/java/ru/example/ivtserver/security/token/TokenCollector.java
+++ src/main/java/ru/example/ivtserver/security/token/TokenCollector.java
@@ -1,4 +1,29 @@
 package ru.example.ivtserver.security.token;
 
-public interface TokenService {
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.lang.NonNull;
+
+public interface TokenCollector {
+
+    @NonNull
+    String generateToken(@NonNull String email);
+
+    @NonNull
+    String generateToken(@NonNull String email, @NonNull Long time);
+
+    @NonNull
+    String getToken(@NonNull HttpServletRequest request);
+
+    @NonNull
+    String getToken(@NonNull String token);
+
+    boolean isValidToken(@NonNull String token);
+
+    @NonNull
+    String getEmailFromToken(@NonNull String token);
+
+    @NonNull
+    Claims getBody(@NonNull String token);
+
 }
